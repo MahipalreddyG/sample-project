82501f47330a8158305eb2f6207fcefda5193230
MahipalreddyG <37070078+MahipalreddyG@users.noreply.github.com>
2018-09-26 17:59:14 +0530

    Create couchdb.sh

---
+++ couchdb.sh
@@ -0,0 +1,3 @@
+echo "deb https://apache.bintray.com/couchdb-deb xenial main" \ | sudo tee -a /etc/apt/sources.list
+curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc \ | sudo apt-key add -
+sudo apt-get install couchdb -y
